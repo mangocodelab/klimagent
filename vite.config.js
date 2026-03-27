507ef9b0e5afc9def9d60ca65b7f890ee306cd97
mangocodelab <eternityaiverify@gmail.com>
2026-03-28 12:01:35 +1300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
