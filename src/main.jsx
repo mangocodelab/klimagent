f6f52e24daf6173e37619f7bf8593323ba3cafec
mangocodelab <eternityaiverify@gmail.com>
2026-03-30 17:22:48 +1300

    Add src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import SnakeGame from './components/SnakeGame.jsx' // Import the SnakeGame component
+import { App } from './App.jsx' // Import the main App component
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    <SnakeGame />
+  </React.StrictMode>
 )
