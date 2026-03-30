363ab9a3cb80c9e37a9dddc152c70e519294c13a
mangocodelab <eternityaiverify@gmail.com>
2026-03-30 16:56:28 +1300

    Update src/email_validator.py

--- src/email_validator.py
+++ src/email_validator.py
@@ -1,4 +1,9 @@
 def validate_email(email):
+    """
+    Validates an email address by checking for @ and . characters
+    :param email: Email address to validate
+    :return: True if valid, False otherwise
+    """
     if '@' in email and '.' in email:
         return True
     return False
